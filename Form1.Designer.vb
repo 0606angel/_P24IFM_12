20ea99cdef54c51b14b1636fcad54dcf1e8df2c8
angel <angel@ANGELA2004>
2024-05-24 09:00:05 +0200

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,117 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btn_month_and_ransomware = New System.Windows.Forms.Button()
+        Me.grd_ransomware = New UJGrid.UJGrid()
+        Me.btn_month_and_incidents = New System.Windows.Forms.Button()
+        Me.btn_register_ransomware_info = New System.Windows.Forms.Button()
+        Me.btn_calc_total = New System.Windows.Forms.Button()
+        Me.btn_calc_average = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btn_month_and_ransomware
+        '
+        Me.btn_month_and_ransomware.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_month_and_ransomware.Location = New System.Drawing.Point(13, 13)
+        Me.btn_month_and_ransomware.Name = "btn_month_and_ransomware"
+        Me.btn_month_and_ransomware.Size = New System.Drawing.Size(278, 48)
+        Me.btn_month_and_ransomware.TabIndex = 0
+        Me.btn_month_and_ransomware.Text = "set grid"
+        Me.btn_month_and_ransomware.UseVisualStyleBackColor = True
+        '
+        'grd_ransomware
+        '
+        Me.grd_ransomware.FixedCols = 1
+        Me.grd_ransomware.FixedRows = 1
+        Me.grd_ransomware.Location = New System.Drawing.Point(298, 13)
+        Me.grd_ransomware.Margin = New System.Windows.Forms.Padding(4)
+        Me.grd_ransomware.Name = "grd_ransomware"
+        Me.grd_ransomware.Scrollbars = System.Windows.Forms.ScrollBars.Both
+        Me.grd_ransomware.Size = New System.Drawing.Size(980, 405)
+        Me.grd_ransomware.TabIndex = 1
+        '
+        'btn_month_and_incidents
+        '
+        Me.btn_month_and_incidents.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_month_and_incidents.Location = New System.Drawing.Point(12, 67)
+        Me.btn_month_and_incidents.Name = "btn_month_and_incidents"
+        Me.btn_month_and_incidents.Size = New System.Drawing.Size(278, 64)
+        Me.btn_month_and_incidents.TabIndex = 2
+        Me.btn_month_and_incidents.Text = "Register months and incidents"
+        Me.btn_month_and_incidents.UseVisualStyleBackColor = True
+        '
+        'btn_register_ransomware_info
+        '
+        Me.btn_register_ransomware_info.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_register_ransomware_info.Location = New System.Drawing.Point(12, 137)
+        Me.btn_register_ransomware_info.Name = "btn_register_ransomware_info"
+        Me.btn_register_ransomware_info.Size = New System.Drawing.Size(278, 45)
+        Me.btn_register_ransomware_info.TabIndex = 3
+        Me.btn_register_ransomware_info.Text = "register ransomware infomation"
+        Me.btn_register_ransomware_info.UseVisualStyleBackColor = True
+        '
+        'btn_calc_total
+        '
+        Me.btn_calc_total.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_calc_total.Location = New System.Drawing.Point(13, 188)
+        Me.btn_calc_total.Name = "btn_calc_total"
+        Me.btn_calc_total.Size = New System.Drawing.Size(278, 51)
+        Me.btn_calc_total.TabIndex = 4
+        Me.btn_calc_total.Text = "calculate total "
+        Me.btn_calc_total.UseVisualStyleBackColor = True
+        '
+        'btn_calc_average
+        '
+        Me.btn_calc_average.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_calc_average.Location = New System.Drawing.Point(13, 246)
+        Me.btn_calc_average.Name = "btn_calc_average"
+        Me.btn_calc_average.Size = New System.Drawing.Size(278, 49)
+        Me.btn_calc_average.TabIndex = 5
+        Me.btn_calc_average.Text = "Calculate Average"
+        Me.btn_calc_average.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1320, 611)
+        Me.Controls.Add(Me.btn_calc_average)
+        Me.Controls.Add(Me.btn_calc_total)
+        Me.Controls.Add(Me.btn_register_ransomware_info)
+        Me.Controls.Add(Me.btn_month_and_incidents)
+        Me.Controls.Add(Me.grd_ransomware)
+        Me.Controls.Add(Me.btn_month_and_ransomware)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btn_month_and_ransomware As Button
+    Friend WithEvents grd_ransomware As UJGrid.UJGrid
+    Friend WithEvents btn_month_and_incidents As Button
+    Friend WithEvents btn_register_ransomware_info As Button
+    Friend WithEvents btn_calc_total As Button
+    Friend WithEvents btn_calc_average As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,233 @@
+﻿Option Strict On
+Option Infer Off
+Option Explicit On
+
+
+Public Class Form1
+
+    Private Structure ransomware_rec
+        Public ransomware_name As String
+        Public ransomware_paid_per_incidents() As Integer
+        Public total_ransomware_paid As Integer
+    End Structure
+
+    Private Structure month_rec
+        Public month_name As String
+        Public total_incidents_of_cyberattack As Integer
+        Public ransom_info() As ransomware_rec
+        Public average As Double
+        Public impact As String
+    End Structure
+
+    Private monthly_info() As month_rec
+
+
+    Private Sub set_grid()
+
+        ' Ask user for the number of months, ransomware, and incidents
+        Dim Month_input As Integer = CInt(InputBox("Enter the number of months"))
+        Dim ransomware_input As Integer = CInt(InputBox("Enter the number of ransomware"))
+        Dim incidence As Integer = CInt(InputBox("Enter the number of incidents"))
+
+        ReDim monthly_info(Month_input - 1)
+
+        For i As Integer = 0 To Month_input - 1
+            ReDim monthly_info(i).ransom_info(ransomware_input - 1)
+            For r As Integer = 0 To ransomware_input - 1
+                ReDim monthly_info(i).ransom_info(r).ransomware_paid_per_incidents(incidence - 1)
+            Next
+        Next
+
+        grd_ransomware.Rows = Month_input + 1
+        grd_ransomware.Cols = ransomware_input + 4
+
+        For i As Integer = 1 To Month_input
+            grd_ransomware.Row = i
+            grd_ransomware.Col = 0
+            grd_ransomware.Text = "Month " + CStr(i)
+        Next
+
+        For r As Integer = 1 To ransomware_input
+            grd_ransomware.Row = 0
+            grd_ransomware.Col = r
+            grd_ransomware.Text = "Ransomware " + CStr(r)
+        Next
+
+        grd_ransomware.Row = 0
+        grd_ransomware.Col = ransomware_input + 1
+        grd_ransomware.Text = "total"
+
+        grd_ransomware.Row = 0
+        grd_ransomware.Col = ransomware_input + 2
+        grd_ransomware.Text = "average"
+
+        grd_ransomware.Row = 0
+        grd_ransomware.Col = ransomware_input + 3
+        grd_ransomware.Text = "impact"
+
+    End Sub
+
+
+    Private Sub btn_month_and_ransomware_Click(sender As Object, e As EventArgs) Handles btn_month_and_ransomware.Click
+
+        set_grid()
+
+    End Sub
+
+    Private Sub btn_month_and_incidents_Click(sender As Object, e As EventArgs) Handles btn_month_and_incidents.Click
+
+        For i As Integer = 0 To monthly_info.Length - 1
+
+            Dim month_name_input As String = InputBox("Enter the name of the month")
+
+            If month_name_input <> "" Then
+                monthly_info(i).month_name = month_name_input
+
+            End If
+
+            Dim total_incidents_input As Integer = CInt(InputBox("Enter the total incidents of cyberattacks"))
+
+            If CStr(total_incidents_input) <> "" Then
+                monthly_info(i).total_incidents_of_cyberattack = total_incidents_input
+            End If
+
+        Next
+        update_grid()
+    End Sub
+
+    Private Sub update_grid()
+        For i As Integer = 0 To monthly_info.Length - 1
+
+            grd_ransomware.Row = i + 1
+            grd_ransomware.Col = 0
+            grd_ransomware.Text = monthly_info(i).month_name
+
+            For m As Integer = 0 To monthly_info(i).ransom_info.Length - 1
+
+                grd_ransomware.Row = 0
+                grd_ransomware.Col = m + 1
+                grd_ransomware.Text = monthly_info(i).ransom_info(m).ransomware_name
+
+
+                For r As Integer = 0 To monthly_info(i).ransom_info(m).ransomware_paid_per_incidents.Length - 1
+                    grd_ransomware.Row = i + 1 ' Update row index
+                    grd_ransomware.Col = m + 1 ' Update column index
+                    grd_ransomware.Text = CStr(monthly_info(i).ransom_info(m).ransomware_paid_per_incidents(r))
+
+                Next
+
+            Next
+        Next
+
+        For i As Integer = 0 To monthly_info.Length - 1
+            For b As Integer = 0 To monthly_info(i).ransom_info.Length - 1
+                grd_ransomware.Col = monthly_info(i).ransom_info.Length + 1
+                grd_ransomware.Row = i + 1
+                grd_ransomware.Text = CStr(monthly_info(i).ransom_info(b).total_ransomware_paid)
+
+            Next
+        Next
+
+        For i As Integer = 0 To monthly_info.Length - 1
+            For c As Integer = 0 To monthly_info(i).ransom_info.Length - 1
+                grd_ransomware.Row = i + 2
+                grd_ransomware.Col = monthly_info(i).ransom_info.Length + 1
+                grd_ransomware.Text = CStr(monthly_info(i).average)
+
+
+            Next
+
+        Next
+
+
+    End Sub
+
+    Private Sub btn_register_ransomware_info_Click(sender As Object, e As EventArgs) Handles btn_register_ransomware_info.Click
+        For i As Integer = 0 To monthly_info.Length - 1
+
+            For m As Integer = 0 To monthly_info(i).ransom_info.Length - 1
+                Dim ransomware_name_input As String = InputBox("Enter the name of the ransomware")
+
+                If ransomware_name_input <> "" Then
+                    monthly_info(i).ransom_info(m).ransomware_name = ransomware_name_input
+                End If
+
+                For r As Integer = 0 To monthly_info(i).ransom_info(m).ransomware_paid_per_incidents.Length - 1
+                    Dim ransomware_paid_input As Integer = CInt(InputBox("Enter the ransomware paid per incidents"))
+                    If CStr(ransomware_paid_input) <> "" Then
+                        monthly_info(i).ransom_info(m).ransomware_paid_per_incidents(r) = ransomware_paid_input
+
+                    End If
+
+                Next
+
+            Next
+
+        Next
+
+        update_grid()
+    End Sub
+
+    Private Sub btn_calc_total_Click(sender As Object, e As EventArgs) Handles btn_calc_total.Click
+        Dim total As Integer = 0
+
+        For t As Integer = 0 To monthly_info.Length - 1
+            For m As Integer = 0 To monthly_info(t).ransom_info.Length - 1
+                total = monthly_info(t).ransom_info(m).ransomware_paid_per_incidents.Sum
+                monthly_info(t).ransom_info(m).total_ransomware_paid = total
+
+            Next
+
+
+
+
+        Next
+        update_grid()
+
+
+    End Sub
+
+    Private Function calc_sum(ByVal sum_array() As Integer) As Integer
+        Dim sum As Integer = 0
+
+
+        For t As Integer = 0 To sum_array.Length - 1
+            sum = sum + sum_array(t)
+
+
+
+
+        Next
+        Return sum
+
+
+    End Function
+
+    Private Sub btn_calc_average_Click(sender As Object, e As EventArgs) Handles btn_calc_average.Click
+
+        Dim average As Double = 0
+        For j As Integer = 0 To monthly_info.Length - 1
+            For h As Integer = 0 To monthly_info(j).ransom_info.Length - 1
+                average = calc_ave(monthly_info(j).ransom_info(h).ransomware_paid_per_incidents)
+
+                monthly_info(j).average = average
+            Next
+
+
+        Next
+        update_grid()
+
+
+    End Sub
+
+    Private Function calc_ave(ByVal ave_array() As Integer) As Double
+
+        Dim total As Integer = calc_sum(ave_array)
+
+
+        For i As Double = 0 To ave_array.Length - 1
+
+            Return total / ave_array.Length - 1
+        Next
+    End Function
+End Class
